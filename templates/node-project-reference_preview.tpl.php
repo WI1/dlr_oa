32512dde97576336142f8dd0c6500c8f4449fad0
Dominik Böhler <dominik.boehler@wiso.uni-erlangen.de>
2012-06-29 14:55:23 +0200

    used correct path for imagecache theming

--- templates/node-project-reference_preview.tpl.php
+++ templates/node-project-reference_preview.tpl.php
@@ -1,5 +1,5 @@
 <?php
 ?>
 
-<p><?php print theme('imagecache','user-m',$node->field_projectlogo[0]['view']); ?></p>
+<p><?php print theme('imagecache','user-m',$node->field_projectlogo[0]['filepath']); ?></p>
 <h3><?php print $node->title; ?></h3>
