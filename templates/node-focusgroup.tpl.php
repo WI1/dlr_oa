d4ebd37d327e38d079481d3fccb431dfcbee19ac
Dominik Böhler <dominik.boehler@wiso.uni-erlangen.de>
2012-07-18 13:44:46 +0200

    styles for atrium folders

--- templates/node-focusgroup.tpl.php
+++ templates/node-focusgroup.tpl.php
@@ -5,7 +5,7 @@
  <dt>Betreuung DLR</br>
 <?php print $node->field_contactdlrpicture[0]['view'] ?></dt>
  <dd><?php print $node->field_contactdlr[0]['view'] ?></dd>
-<dt>Betreuung BALANCE</dt>
+<dt>Betreuung DemoScreen</dt>
   <dd><?php print phptemplate_business_card($node->field_contactbalance[0]['uid']); ?></dd>
 <dt>Projekte</dt>
   <dd><?php print $node->field_projects[0]['view']; print views_embed_view('dlr_groups_fg_projects_grid', 'page_1', $node->nid); ?></dd>
