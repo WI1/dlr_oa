06955ea32b5495e7535427f35cbf6855e64b11fb
Dominik Böhler <dominik.boehler@wiso.uni-erlangen.de>
2012-08-02 08:38:01 +0200

    moved context links back to page tools node title will stay left

--- page.tpl.php
+++ page.tpl.php
@@ -10,12 +10,6 @@
   <?php print $content_region ?>
 </div></div>
 <div id='right'><div class='page-region'>
-      <?php if($active_space->type == "og" && isset($node) && $node->type != $active_space->group->type):?>
-        <?php if ($context_links): ?><div class='context-links'><?php print $context_links ?></div><?php endif; ?>
-      <?php else : ?>	
-      <?php if ($context_links): ?><div class='context-links'><?php print $context_links ?></div><?php endif; ?>
-      <?php if ($extrablanklines): // For better formatting of User Profile?><div ><?php print $extrablanklines ?></div><?php endif; ?>
-   	 <?php endif; ?>
   <?php if ($right) print $right ?>
 </div></div>
 
