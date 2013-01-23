26873268c88109b1630e7088c6402829879d399b
Philipp <philipp.klinger@wi1.wiso.uni-erlangen.de>
2013-01-23 18:10:29 +0100

    ..

--- templates/node-organisation-reference_preview.tpl.php
+++ templates/node-organisation-reference_preview.tpl.php
@@ -1,5 +1,9 @@
 <?php
-?>
 
+  // "Reference preview build mode" laut view-liste in drupal
+
+  // laut Screenshots von ende Januar 2013 soll der Link unter dem Bild stehen...
+  // mal schauen wie lange das gilt...
+?>
 <p><?php print theme('imagecache', 'projectlogo-m', $node->field_companylogo[0]['filepath']); ?></p>
-<p><?php print $node->title; ?></p>
+<p><a href="<?php print $node_url; ?>"><?php print $node->title; ?></a></p>
--- templates/node-project-reference_preview.tpl.php
+++ templates/node-project-reference_preview.tpl.php
@@ -2,4 +2,4 @@
 ?>
 
 <p><?php print theme('imagecache','projectlogo-m',$node->field_projectlogo[0]['filepath']); ?></p>
-<p><?php print $node->title; ?></p>
+<p><?php print $node->title;?></p>
