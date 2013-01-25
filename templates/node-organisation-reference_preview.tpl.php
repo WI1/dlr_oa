ebf56a3a8968100534852dfd3e6baf449977c3c2
Philipp <philipp.klinger@wi1.wiso.uni-erlangen.de>
2013-01-25 17:05:24 +0100

    projekte nebeneinander anzeigen

--- templates/node-organisation-reference_preview.tpl.php
+++ templates/node-organisation-reference_preview.tpl.php
@@ -5,5 +5,11 @@
   // laut Screenshots von ende Januar 2013 soll der Link unter dem Bild stehen...
   // mal schauen wie lange das gilt...
 ?>
-<p><?php print theme('imagecache', 'projectlogo-m', $node->field_companylogo[0]['filepath']); ?></p>
-<p><a href="<?php print $node_url; ?>"><?php print $node->title; ?></a></p> 
+
+
+<table>
+  <tr>
+    <td><?php print theme('imagecache', 'projectlogo-m', $node->field_companylogo[0]['filepath']); ?></td>
+    <td valign="middle"><a href="<?php print $node_url; ?>"><?php print $node->title; ?></a></td>
+</tr>
+</table>
