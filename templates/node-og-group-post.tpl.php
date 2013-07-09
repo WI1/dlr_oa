f1ed6f47666fc2708b516c1d5e82e167487bc072
Phil <philipp.klinger@wi1.wiso.uni-erlangen.de>
2013-07-09 11:48:22 +0200

    conflict

--- templates/node-og-group-post.tpl.php
+++ templates/node-og-group-post.tpl.php
@@ -1,7 +1,7 @@
 <?php if(isset($before)) { print $before; }?>
 <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?><?php if (isset($og_public) && $og_public === FALSE) { print ' node-private'; } ?> <?php if (isset($node_classes)) { print $node_classes; } ?> node-<?php print $type; ?> clear-block">
 		<?php if (!$page): ?>
-		<h2 class="node-title"><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+		<h2 id="ueberschriftmetaprojekth2" class="node-title"><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
 	<?php endif; ?>
 	<?php if(!$page && isset($focusgroups)): ?>
 		<div class="fg">
